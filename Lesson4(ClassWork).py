528dbecf1170952aac682669e9b75d8499b3acb9
Igor <yudin-igor@rambler.ru>
2019-04-07 15:21:29 +0300

    Добавил код игры

--- Lesson4(ClassWork).py
+++ Lesson4(ClassWork).py
@@ -31,3 +31,25 @@ elif count > 0:
     print("Число больше нуля!")
 else:
     print("Равно нулю!")
+
+print("-------------------------------------------- Игра угадай число от 1 до 20 (5 попыток) ")
+
+import random
+
+number = random.randint(1, 20)  # Рандом от 1 до 20
+guesses = 0
+while guesses < 5:
+    print('Напишите число от 1 до 20: ')
+    guess = int(input())
+    guesses += 1
+    if guess == number:
+        print('Успех!')
+    if guess < number:
+        print('Число слишком маленькое. Попробуй еще.')
+    if guess > number:
+        print('Число слишком большое. Попробуй еще.')
+    if guesses == 5:
+        print('Ты не угадал число. Исходное число: ' + str(number))
+    if guess == number:
+        print('Вы угадали число. ' + str(guesses))
+        break
