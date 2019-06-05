384a097355b712bab6850d1c4401de7803cd1878
Igor <yudin-igor@rambler.ru>
2019-06-05 15:18:19 +0300

    Готово (без badoo)

--- Lesson13(MongoDB)/Lesson13(ClassWork).py
+++ Lesson13(MongoDB)/Lesson13(ClassWork).py
@@ -1,28 +1,47 @@
-from ClassLawrMongoDB import MongoDB
+from ClassLawr_MongoDB import MongoDB
 
-Test = MongoDB("mongodb+srv://Admin:P13hesheshes@botonlnr-h6f2t.mongodb.net/test?retryWrites=true&w=majority")
+# Используется локальная база данных на MacOS
+# Для просмотра используется: MongoDB Compass
 
+print("-------------------------------------------- Тестирование своего класса MongoDB")
 
-Test.nice_print(Test.show_database('Test'))
-print(Test.check_collection('Test', 'URL'))
+# string = "mongodb+srv://Admin:P13hesheshes@botonlnr-h6f2t.mongodb.net/test?retryWrites=true&w=majority"
+string = "localhost:27017"
 
-Test.clear_collection('Test', 'URL')
+# Подключение
+print()
+BD = MongoDB(connection_string=string)
 
-Test.add_line_to_end('Test', 'URL', {'name': 'user 1', 'level': 1})
-Test.add_line_to_end('Test', 'URL', {'name': 'user 2', 'level': 2})
-Test.add_line_to_end('Test', 'URL', {'name': 'user 3', 'level': 3})
+# Удаление Коллекции (если не существует ничего не происходит)
+print()
+BD.delete_collection(dbs='ClassLawr_MongoDB', collection='OneCollection')
+BD.delete_collection(dbs='ClassLawr_MongoDB', collection='TwoCollection')
 
-Test.nice_print(Test.show_collection('Test', 'URL'))
+# Создание Базы данных + Коллекция (При добавлении данных создается автоматически)
+print()
+BD.add_line_to_end('ClassLawr_MongoDB', 'OneCollection', {'name': 'user 1', 'level': 1})
+BD.add_line_to_end('ClassLawr_MongoDB', 'OneCollection', {'name': 'user 2', 'level': 2})
+BD.add_line_to_end('ClassLawr_MongoDB', 'TwoCollection', {'name': 'user 3', 'level': 3})
+BD.add_line_to_end('ClassLawr_MongoDB', 'TwoCollection', {'name': 'user 4', 'level': 4})
+BD.add_line_to_end('ClassLawr_MongoDB', 'TwoCollection', {'name': 'user 5', 'level': 5})
 
-Test.delete_collection('Test', '111')
-Test.clear_collection('Test', 'URL')
-Test.nice_print(Test.show_collection('Test', 'URL'))
+# Показать что содержиться внутри "database"
+print()
+BD.nice_print(BD.show_database(dbs='ClassLawr_MongoDB'))
 
+# Показать что содержиться внутри "collection"
+print()
+BD.nice_print(BD.show_collection(dbs='ClassLawr_MongoDB', collection='OneCollection'))
+BD.nice_print(BD.show_collection(dbs='ClassLawr_MongoDB', collection='TwoCollection'))
 
-Test.add_line_to_end('Test', 'URL', {'name': 'user 1', 'level': 1})
-Test.add_line_to_end('Test', 'URL', {'name': 'user 2', 'level': 2})
-Test.add_line_to_end('Test', 'URL', {'name': 'user 3', 'level': 3})
+# Проверка существует ли Коллекци (True, False)
+print()
+print("-> Существует?", BD.check_collection(dbs='ClassLawr_MongoDB', name_collection='OneCollection'))
 
-Test.del_line_to_end('Test', 'URL')
-Test.nice_print(Test.show_collection('Test', 'URL'))
+# Удаление всех документов в коллекции
+print()
+BD.clear_collection(dbs='ClassLawr_MongoDB', collection='TwoCollection')
 
+# Удаление последнего документа в коллекции
+print()
+BD.del_line_to_end(dbs='ClassLawr_MongoDB', collection='OneCollection')
