1ac03bf6266e8e07a8fd5bb5251872ac40bb2ca6
Igor <yudin-igor@rambler.ru>
2019-06-29 14:37:06 +0300

    Готово

--- Lesson13(MongoDB)/Lesson13(HomeWork).py
+++ Lesson13(MongoDB)/Lesson13(HomeWork).py
@@ -1,5 +1,8 @@
 from ClassLawr_MongoDB import MongoDB
 import Lesson07LawrParserNBRB
+from selenium import webdriver
+from time import sleep
+import os
 
 print("-------------------------------------------- Задание 1 (67)")
 
@@ -39,3 +42,40 @@ for i in range(20 + 1):  # Цикл от 0 до 20
     BD.nice_print(BD.show_collection('Lesson13(HomeWork)', name_collection))
 
     del rates  # Удаляем объект (так как в цикле он будет создаваться заново)
+
+print("-------------------------------------------- Задание 2 (86)")
+
+'''
+Написать бота для сайта знакомств "Badoo" при помощи Selenium, 
+который бы имитировал действия пользователя (ставил сердечки или кресты)
+https://www.youtube.com/watch?v=FKwbxMP8yko&feature=youtu.be
+'''
+
+# Задание путей
+executable_path = "driver_chrome/chromedriver_mac64"
+browser = webdriver.Chrome(executable_path=executable_path)
+# --АВТОРИЗАЦИЯ--
+browser.get("https://badoo.com/ru/signin/")
+textarea = browser.find_element_by_name('email')
+textarea.send_keys('interes46@mail.ru')
+textarea = browser.find_element_by_name('password')
+textarea.send_keys('lydaruqa')
+submit = browser.find_element_by_name('post')
+submit.click()
+# --ДЕЙСТВИЯ лайк/дизлайк--
+sleep(10)
+flag = True
+for i in range(5):
+    sleep(4)  # увеличить при ошибке (попробывать лайк в ручную)
+    # css_selector = .xxx (на примере: class="xxx yyy zzz")
+    if flag:
+        submit = browser.find_element_by_css_selector('.profile-action--yes')
+        flag = False
+    else:
+        submit = browser.find_element_by_css_selector('.js-profile-header-vote-no')
+        flag = True
+    submit.click()
+browser.quit()
+os.remove("geckodriver.log")  # Удаление файла "geckodriver.log"
+
+
